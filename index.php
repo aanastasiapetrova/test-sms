662b49853cc939db4213b814fe3f5b741ca4b64e
Anastasia Petrova <71938351+aanastasiapetrova@users.noreply.github.com>
2021-08-02 13:44:17 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<?php
+
+echo 'fksnbk jksjkn knnzklxblks';
+
+include $_SERVER['DOCUMENT_ROOT'] . '/qtsms.class/QTSMS.class.php';
+//include $_SERVER['DOCUMENT_ROOT'] . '/qtsms.class/test_config.php';
+
+$cfg = array(
+    'login' => '1690601',					// ваш логин в системе
+    'password' => '16906011',				// ваш пароль
+    'host' => 'https://a2p-sms.beeline.ru'		// хост для доступа к сервису
+);
+
+$sms_text = 'test sms';
+$phone_num = '+79170615202';
+
+$iwix_sms_name = 'LogicT';
+$iwix_sms_period = 600;
+
+$qtsms = new Qtsms($cfg['login'], $cfg['password'], $cfg['host']);
+$result = $qtsms->post_message($sms_text, $phone_num, $phone_num, 'x124127456', $iwix_sms_period);
+
+//header("Content-Type: text/xml; charset=UTF-8");
+echo "hello";
+?>
+
